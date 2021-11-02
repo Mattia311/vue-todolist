a77bea310ecc70606228e05b5423804f9715bb90
Mattia311 <mattia.space98@gmail.com>
2021-11-02 14:34:56 +0100

    es complete

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+/*stampare in pagina un item per ogni elemento contenuto in un array ogni item ha una "x" associata: 
+cliccando su di essa, l'item viene rimosso dalla lista 
+predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, 
+il testo digitato viene aggiunto alla lista*/
+
+
+/*La nostra todo list avrá alcune tasks di default predefinite
+ L'utente puó inserire nuove tasks
+ Cliccando sulla "X" l'utente puó cancellare una task
+ Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
+ ma vuole anche poter indicare che la task é stata completata (con un icona cliccabile)*/
+
+ 
+
+ 
+ 
+ 
+const app = new Vue ({
+    el: '#app',
+    data: {
+        img: 'https://www.boolean.careers/images/misc/logo.png',
+
+        newTask: "",
+        
+        tasks: [
+ 
+            'fare i compiti',
+            'fare la spesa',
+            'fare il bucato',
+          
+        ],
+        errore: false
+    },
+    methods: {
+        remove (i) {
+            this.tasks.splice(i,1)
+        },
+
+        add () {
+            console.log('aggiungi');
+            if(this.newTask != '' && this.newTask.length > 10){
+                this.tasks.push(this.newTask)
+                this.errore = false
+            } else {
+                this.errore = true
+
+            }
+
+            this.newTask = ''
+        }
+    }
+})
